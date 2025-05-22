70ecd8e562ad593d5f31983209352143031deae8
abhishek-rc <abhishekkumar@royalcyber.com>
2025-05-22 21:55:03 +0530

    latest upto seller quote list

--- b2b-quotes-graphql/node/templates/index.ts
+++
@@ -1,7 +0,0 @@
-import { quoteCreatedMessage } from './quoteCreated'
-import { quotePlacedMessage } from './quotePlaced'
-import { quoteUpdatedMessage } from './quoteUpdated'
-
-const templates = [quoteCreatedMessage, quotePlacedMessage, quoteUpdatedMessage]
-
-export default templates
